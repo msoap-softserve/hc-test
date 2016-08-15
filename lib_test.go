c13d568b618fac64afa3b049ab4ba9b24a370488
Serhii Mudryk <smud@softserveinc.com>
2016-08-15 11:43:11 +0300

    Added test cases

--- lib_test.go
+++ lib_test.go
@@ -3,4 +3,34 @@ package hchat
 import "testing"
 
 func TestParse(t *testing.T) {
+	testCases := []struct {
+		msg  string
+		json string
+		err  error
+	}{
+		{
+			"",
+			"{}",
+			nil,
+		}, {
+			"simple message",
+			"{}",
+			nil,
+		},
+	}
+
+	for i, item := range testCases {
+		json, err := Parse(item.msg)
+
+		if err != nil && item.err == nil {
+			t.Errorf("%d. Got error: %s", i, err)
+		}
+		if err == nil && item.err != nil {
+			t.Errorf("%d. Not got error", i)
+		}
+
+		if json != item.json {
+			t.Errorf("%d. expected: %#v, real: %#v", i, item.json, json)
+		}
+	}
 }
